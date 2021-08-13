bb94dddbd8ccd2b5261cd948de2bf35990a8da62
equaliser0 <accounts@equaliser.minimalcity.de>
2021-08-13 17:32:12 +0200

    update page & add posts

--- _nuxt/63a4748.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{224:function(e,n,t){"use strict";t.r(n);var c={},r=t(33),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div",{})}),[],!1,null,"00c49c41",null);n.default=component.exports}}]);
